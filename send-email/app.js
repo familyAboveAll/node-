b97c52063860b9d2f57ed3316934d4e2df2c910e
ymy <yangmengyan@thunder.com.cn>
2018-11-22 15:03:15 +0800

    发送邮件

--- send-email/app.js
+++ send-email/app.js
@@ -1,10 +1,10 @@
 var email = require('nodemailer')
 var config = {
-  host: 'smtp.sina.com',
+  host: 'smtp.sina.com', //SMTP是发送邮件的协议
   port: 25,
   auth: {
-    user: '',
-    pass: ''
+    user: '', //发件人邮箱
+    pass: '' //发件人邮箱密码
   }
 }
 var transporter = email.createTransport(config)
@@ -13,8 +13,8 @@ var options = {
   from: 'b13180067319@sina.cn',
   to: '1370437662@qq.com',
   subject: '测试node发送邮件',
-  html: '你好，这是一封来自hoylyn的邮件！',
-  attachments: [{
+  html: '你好，这是一封来自hoylyn的邮件！', //支持html格式
+  attachments: [{ //附件
     filename: 'monstax.jpg',
     path: './imgs/1.jpg'
   }]
